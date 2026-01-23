cb811611a4d7dc49179a1d809620d15a49273460
kirubha-malaiyappan <kirubha205@gmail.com>
2026-01-23 15:08:06 +0530

    Verbatim String

--- Program.cs
+++ Program.cs
@@ -2,22 +2,13 @@
 {
     private static void Main(string[] args)
     {
-        int num1 = 10;
-        int num2 = 20;
-        //Mandatory to modify
-        Console.WriteLine($"Before Swapping {num1} and {num2}");
-        Program.Swap(out num1, out num2);
-        Console.WriteLine($"After Swapping {num1} and {num2}");
+        string s = @"hiii ""kirubha"" {name}";
+        Console.WriteLine(s);
 
 
     }
 
-    private static void Swap(out int x, out int y)
-    {
-        int temp = x;
-        x = y;
-        y = temp;
-    }
+  
 
 
 }
